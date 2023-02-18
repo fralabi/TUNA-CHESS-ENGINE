bd6dae91645cd0fdc2bd59b316c2e2f2dd3bf872
La Barbera Francesco <labarberafrancesco01@gmail.com>
2023-02-18 21:17:45 +0100

    EDIT bitboard.cpp and CREATE main.cpp

--- bitboard.cpp
+++ bitboard.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 typedef uint64_t Bitboard;
 
 struct ChessBoard
@@ -26,6 +28,43 @@ struct ChessBoard
 	Bitboard AllPieces;
 };
 
-int main() {
-    
+void printChessBoard (ChessBoard chessboard) {
+
+	cout << "   +---+---+---+---+---+---+---+---+" << endl;
+
+	for (int i=7; i>=0; i--) {
+		cout << " " << i+1 << " |";
+		for (int j=0; j<=7; j++) {
+			//CHECK IF Nth BIT IS SET OR NOT
+			if (((chessboard.AllPieces >> (j+i*8)) & 1) > 0)
+				cout << " X |";
+			else
+				cout << "   |";
+		}
+		cout << "\n   +---+---+---+---+---+---+---+---+" << endl;
+	}
+	cout << "     a   b   c   d   e   f   g   h  " << endl;
+
+}
+
+void initializeChessBoard (ChessBoard &chessboard) {
+
+	chessboard.WhitePawns 	= 0x000000000000FF00;
+	chessboard.WhiteRooks 	= 0x0000000000000081;
+	chessboard.WhiteKnights = 0x0000000000000042;
+	chessboard.WhiteBishops = 0x0000000000000024;
+	chessboard.WhiteQueens 	= 0x0000000000000010;
+	chessboard.WhiteKing 	= 0x0000000000000008;
+
+	chessboard.BlackPawns 	= 0x00FF000000000000;
+	chessboard.BlackRooks	= 0x1800000000000000;
+	chessboard.BlackKnights = 0x2400000000000000;
+	chessboard.BlackBishops = 0x4200000000000000;
+	chessboard.BlackQueens	= 0x0100000000000000;
+	chessboard.BlackKing	= 0x8000000000000000;
+
+	chessboard.AllBlackPieces = chessboard.BlackPawns | chessboard.BlackRooks | chessboard.BlackKnights | chessboard.BlackBishops | chessboard.BlackQueens | chessboard.BlackKing;
+	chessboard.AllWhitePieces = chessboard.WhitePawns | chessboard.WhiteRooks | chessboard.WhiteKnights | chessboard.WhiteBishops | chessboard.WhiteQueens | chessboard.WhiteKing;
+
+	chessboard.AllPieces = chessboard.AllBlackPieces | chessboard.AllWhitePieces;
 }
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "bitboard.cpp"
+
+using namespace std;
+
+int main() {
+
+    ChessBoard first;
+	initializeChessBoard(first);
+	printChessBoard(first);
+}
