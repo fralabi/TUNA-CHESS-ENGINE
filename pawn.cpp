10e4cfa62b395843a5b5b2e0a43f8b35634527f9
La Barbera Francesco <labarberafrancesco01@gmail.com>
2023-02-19 11:29:34 +0100

    Create pawn.cpp

---
+++ pawn.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "bitboard.h"
+
+using namespace std;
+
+void promotion (Chessboard &chessboard, int color) { //color = White(0) or Black(1)
+
+}
+
+void move (Chessboard &chessBoard, int color) { //color = White(0) or Black(1)
+
+}
