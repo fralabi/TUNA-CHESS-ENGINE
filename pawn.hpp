9582531f812029ff4f859af5f07310d4054139a4
La Barbera Francesco <labarberafrancesco01@gmail.com>
2023-02-20 15:30:16 +0100

    Update pawn.hpp

--- pawn.hpp
+++ pawn.hpp
@@ -4,58 +4,103 @@
 
 using namespace std;
 
-int promotionWhite (ChessBoard &chessboard) { 
+/*
+NOTAZIONE BITBOARD
 
++----+-----+----+
+|    | +16 |    |
++----+-----+----+
+| +7 |  +8 | +9 |
++----+-----+----+
+| -1 |  0  | +1 |
++----+-----+----+
+| -9 |  -8 | -7 |
++----+-----+----+
+
+VALIDO PER PEDONI E RE
+
+*/
+
+int promotionWhite (ChessBoard &chessboard, Bitboard bitboard) { 
+
+    if (bitboard == 0) {
+        return -0xFFFFFFFFFFFFFFFF;
+    }
+
+    //CALCOLARE PEZZI IN POSIZIONE +8
 }
 
-int captureWhite (ChessBoard &chessboard) {     
+int captureWhite (ChessBoard &chessboard, Bitboard bitboard) {     
 
+    if (bitboard == 0) {
+        return -0xFFFFFFFFFFFFFFFF;
+    }
+
+    //CALCOLARE PEZZI IN POSIZIONE +7, +9
 }
 
-int initialMoveWhite (ChessBoard &chessboard) {
+int initialMoveWhite (ChessBoard &chessboard, Bitboard bitboard) {
 
+    if (bitboard == 0) {
+        return -0xFFFFFFFFFFFFFFFF;
+    }
+
+    //CALCOLARE PEZZI IN POSIZIONE +7, +8, +9, +16
 }
 
 int intermediateRankWhite (ChessBoard &chessboard, Bitboard bitboard) {
 
+    if (bitboard == 0) {
+        return -0xFFFFFFFFFFFFFFFF;
+    }
+
+    //CALCOLARE PEZZI IN POSIZIONE +7, +8, +9
 }
 
 int intermediateRankBlack (ChessBoard &chessboard, Bitboard bitboard) {
     
 }
 
-int promotionBlack (ChessBoard &chessboard) {   
+int promotionBlack (ChessBoard &chessboard, Bitboard bitboard) {   
 
 }
 
-int captureBlack (ChessBoard &chessboard) {    
+int captureBlack (ChessBoard &chessboard, Bitboard bitboard) {    
 
 }
 
-int initialMoveBlack (ChessBoard &chessboard) {
+int initialMoveBlack (ChessBoard &chessboard, Bitboard bitboard) {
 
 }
 
-int validMoveWhite (ChessBoard &chessboard) { 
+int validPawnMoveWhite (ChessBoard &chessboard) { 
 
-    initialMoveWhite(chessboard, chessBoard.WhitePawns | rank[2]);
+    int bestValue = initialMoveWhite(chessboard, chessBoard.WhitePawns & rank[2]);
     
     for (int i=2; i<=6; i++) {
-       intermediateRank(chessboard, chessboard.WhitePawns | rank[i]);
+       int value = intermediateRank(chessboard, chessboard.WhitePawns & rank[i]);
+       if (value > bestValue) {
+        value = temp;
+       }
     }
 
-    promotionWhite(chessboard, chessboard.WhitePawns | rank[7]);
+    int value = promotionWhite(chessboard, chessboard.WhitePawns & rank[7]);
+    if (value > bestValue) {
+        value = temp;
+       }
+
+       return value;
 }
 
-int validMoveBlack (ChessBoard &chessboard) {    
+int validPawnMoveBlack (ChessBoard &chessboard) {    
 
-    initialMoveBlack(chessboard, chessboard.BlackPawns | rank[7]);
+    initialMoveBlack(chessboard, chessboard.BlackPawns & rank[7]);
 
     for (int i=6; i>=2; i--) {
-       intermediateRank(chessboard, chessboard.BlackPawns | rank[i]);
+       intermediateRank(chessboard, chessboard.BlackPawns & rank[i]);
     }
 
-    promotionBlack(chessboard, chessboard.BlackPawns | rank[2]);
+    promotionBlack(chessboard, chessboard.BlackPawns & rank[2]);
 
 }
 
