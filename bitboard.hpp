ce58b957aa17a9761bb9c65a6727f070fb344523
La Barbera Francesco <labarberafrancesco01@gmail.com>
2023-02-22 17:53:07 +0100

    Update evaluate.hpp

--- bitboard.hpp
+++ bitboard.hpp
@@ -84,19 +84,19 @@ void printBitboard (Bitboard bitboard)
 
 void initializeChessBoard (ChessBoard &chessboard) {
 
-	chessboard.WhitePawns 	= 0x000000000000FF00;
-	chessboard.WhiteRooks 	= 0x0000000000000081;
-	chessboard.WhiteKnights = 0x0000000000000042;
-	chessboard.WhiteBishops = 0x0000000000000024;
-	chessboard.WhiteQueens 	= 0x0000000000000010;
-	chessboard.WhiteKing 	= 0x0000000000000008;
+	chessboard.WhitePawns 	= 0x00FF000000000000;
+	chessboard.WhiteRooks	= 0x8100000000000000;
+	chessboard.WhiteKnights = 0x4200000000000000;
+	chessboard.WhiteBishops = 0x2400000000000000;
+	chessboard.WhiteQueens	= 0x0800000000000000;
+	chessboard.WhiteKing	= 0x1000000000000000;
 
-	chessboard.BlackPawns 	= 0x00FF000000000000;
-	chessboard.BlackRooks	= 0x1800000000000000;
-	chessboard.BlackKnights = 0x2400000000000000;
-	chessboard.BlackBishops = 0x4200000000000000;
-	chessboard.BlackQueens	= 0x0100000000000000;
-	chessboard.BlackKing	= 0x8000000000000000;
+	chessboard.BlackPawns 	= 0x000000000000FF00;
+	chessboard.BlackRooks 	= 0x0000000000000081;
+	chessboard.BlackKnights = 0x0000000000000042;
+	chessboard.BlackBishops = 0x0000000000000024;
+	chessboard.BlackQueens 	= 0x0000000000000008;
+	chessboard.BlackKing 	= 0x0000000000000010;
 
 	chessboard.AllBlackPieces = chessboard.BlackPawns | chessboard.BlackRooks | chessboard.BlackKnights | chessboard.BlackBishops | chessboard.BlackQueens | chessboard.BlackKing;
 	chessboard.AllWhitePieces = chessboard.WhitePawns | chessboard.WhiteRooks | chessboard.WhiteKnights | chessboard.WhiteBishops | chessboard.WhiteQueens | chessboard.WhiteKing;
